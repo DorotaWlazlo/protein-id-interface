26cdf481896c318bb222dd15f9ebcdd76722a287
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-08-30 21:53:06 +0200

    Initial commit

---
+++ src/test/java/com/example/proteinidinterface/ProteinIdInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proteinidinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteinIdInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
